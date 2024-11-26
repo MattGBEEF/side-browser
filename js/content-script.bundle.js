f28907d56743ab20992932045f865571245f088e
matthew <matt@gbeef.net>
2024-11-26 18:09:12 +0000

    zsrxdtfvybhn

---
+++ js/content-script.bundle.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!************************************!*\
+  !*** ./src/ContentScript/index.ts ***!
+  \************************************/
+
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoianMvY29udGVudC1zY3JpcHQuYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7OztBQUFhIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vdjJleF9odWIvLi9zcmMvQ29udGVudFNjcmlwdC9pbmRleC50cyJdLCJzb3VyY2VzQ29udGVudCI6WyJcInVzZSBzdHJpY3RcIjtcbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
